a043d7295b9ac04c4a0ff6b1214527e9315755fd
MiaoMiaoMiao3 <N/A>
2020-11-24 12:27:43 -0800

    Add Select and Material-UI

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import 'rc-slider/assets/index.css'; //import first to override existing styles if required
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
 import './NavBar.css';
 
 class NavBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {format: "hex"};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event){
+        this.setState({format: event.target.value});
+        this.props.handleChange(event.target.value);
+    }
     render(){
-        const {level, changeLevel} = this.props
+        const {level, changeLevel} = this.props;
+        const {format} = this.state;
         return(
             <header className="NavBar">
                 <div className="logo">
@@ -30,6 +42,13 @@ class NavBar extends Component{
                             railStyle={{height: '10px'}}
                             onAfterChange={changeLevel}/>
                     </div>
+                </div>
+                <div className="select-container">
+                    <Select value ={format} onChange = {this.handleChange}>
+                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">RGBA - rgb(255,255,255, 1.0)</MenuItem>
+                    </Select>
                 </div>
             </header>
         )
