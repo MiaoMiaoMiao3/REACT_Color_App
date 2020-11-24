42e4f031f4c737107cebb89f4ae6b0282cc1c93f
MiaoMiaoMiao3 <N/A>
2020-11-23 18:43:29 -0800

    Add colorHelpers to generate colors

--- src/Palette.js
+++ src/Palette.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import ColorBox from './ColorBox';
-import './Pallette.css'
+import './Palette.css'
 
-class Pallette extends Component{
+class Palette extends Component{
     render(){
         const colorBoxes = this.props.colors.map(color => (
             <ColorBox background={color.color} name={color.name}/>
         ))
         return(
-            <div className = "Pallette">
+            <div className = "Palette">
                 {/* navbar goes here */}
-                <div className = "Pallette-colors">
+                <div className = "Palette-colors">
                 {colorBoxes}
                 </div>
                 {/* footer eventually */}
@@ -19,4 +19,4 @@ class Pallette extends Component{
     }
 }
 
-export default Pallette
+export default Palette
